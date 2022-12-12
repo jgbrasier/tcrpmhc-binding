32e94f59b3c34b269be805116e7e2f14d3181620
JG Brasier <jbrasier@g.harvard.edu>
2022-12-12 17:21:44 +0800

    add raw vdj/iedb

--- src/models/nettcr.py
+++ src/models/nettcr.py
@@ -27,80 +27,64 @@ class NetTCR(nn.Module):
         n_filters = 16
         hidden_dim = 32
 
-        self.layers = dict()
+        self.pep_conv = nn.ModuleList([Conv1d(in_channels=peptide_len, out_channels=n_filters, kernel_size=2*i+1, padding='same', device=device) for i in range(self.n_kernels)])
+        self.cdra_conv = nn.ModuleList([Conv1d(in_channels=cdra_len, out_channels=n_filters, kernel_size=2*i+1, padding='same', device=device) for i in range(self.n_kernels)])
+        self.cdrb_conv = nn.ModuleList([Conv1d(in_channels=cdrb_len, out_channels=n_filters, kernel_size=2*i+1, padding='same', device=device) for i in range(self.n_kernels)])
+
+        self.pool =GlobalMaxPool1D()
+        self.activation = nn.Sigmoid()
+
         self.lin1 = Linear(in_features=3*self.n_kernels*n_filters, out_features=hidden_dim, device=device)
         self.lin2 = Linear(in_features=hidden_dim, out_features=1, device=device)
-        self.activation = nn.ReLU()
-        for e, l in self._elems.items():
-            self.layers[e] = dict()
-            for i in range(self.n_kernels):
-                self.layers[e][f"conv_{2*i+1}"] = Conv1d(in_channels=l, out_channels=n_filters, kernel_size=2*i+1, padding='same', device=device)
-                self.layers[e][f"pool_{2*i+1}"] = GlobalMaxPool1D()
+
         
-
-
     def forward(self, x):
         x_peptide, x_cdra, x_cdrb = x
 
-        # pep_out_list = []
-        # for n in range(self.n_kernels):
-        #     pep_out_n = self.activation(self.layers['peptide'][f"conv_{2*n+1}"](x_peptide))
-        #     pep_out_n = self.layers['peptide'][f"pool_{2*n+1}"](pep_out_n)
-        #     pep_out_list.append(pep_out_n)
+        pep_out_list = [self.pool(self.activation(self.pep_conv[n](x_peptide))) for n in range(self.n_kernels)]
+        cdra_out_list = [self.pool(self.activation(self.cdra_conv[n](x_cdra))) for n in range(self.n_kernels)]
+        cdrb_out_list = [self.pool(self.activation(self.cdrb_conv[n](x_cdrb))) for n in range(self.n_kernels)]
 
-        # cdra_out_list = []
-        # for n in range(self.n_kernels):
-        #     cdra_out_n = self.activation(self.layers['cdra'][f"conv_{2*n+1}"](x_cdra))
-        #     cdra_out_n = self.layers['cdra'][f"pool_{2*n+1}"](cdra_out_n)
-        #     cdra_out_list.append(cdra_out_n)
+        pep_out = torch.cat(pep_out_list, dim=1)
+        cdra_out = torch.cat(cdra_out_list, dim=1)
+        cdrb_out = torch.cat(cdrb_out_list, dim=1)
 
-        # cdrb_out_list = []
-        # for n in range(self.n_kernels):
-        #     cdrb_out_n = self.activation(self.layers['cdrb'][f"conv_{2*n+1}"](x_cdrb))
-        #     cdrb_out_n = self.layers['cdrb'][f"pool_{2*n+1}"](cdrb_out_n)
-        #     cdrb_out_list.append(cdrb_out_n)
+        # pep_out_1 = self.activation(self.pep_conv[0](x_peptide))
+        # pep_out_1 = self.pool(pep_out_1)
+        # pep_out_3 = self.activation(self.pep_conv[1](x_peptide))
+        # pep_out_3 = self.pool(pep_out_3)
+        # pep_out_5 = self.activation(self.pep_conv[2](x_peptide))
+        # pep_out_5 = self.pool(pep_out_5)
+        # pep_out_7 = self.activation(self.pep_conv[3](x_peptide))
+        # pep_out_7 = self.pool(pep_out_7)
+        # pep_out_9 = self.activation(self.pep_conv[4](x_peptide))
+        # pep_out_9 = self.pool(pep_out_9)
 
-        # pep_out = torch.cat(pep_out_list, dim=1)
-        # cdra_out = torch.cat(cdra_out_list, dim=1)
-        # cdrb_out = torch.cat(cdrb_out_list, dim=1)
+        # cdra_out_1 = self.activation(self.cdra_conv[0](x_cdra))
+        # cdra_out_1 = self.pool(cdra_out_1)
+        # cdra_out_3 = self.activation(self.cdra_conv[1](x_cdra))
+        # cdra_out_3 = self.pool(cdra_out_3)
+        # cdra_out_5 = self.activation(self.cdra_conv[2](x_cdra))
+        # cdra_out_5 = self.pool(cdra_out_5)
+        # cdra_out_7 = self.activation(self.cdra_conv[3](x_cdra))
+        # cdra_out_7 = self.pool(cdra_out_7)
+        # cdra_out_9 = self.activation(self.cdra_conv[4](x_cdra))
+        # cdra_out_9 = self.pool(cdra_out_9)
 
-        pep_out_1 = self.activation(self.layers['peptide']["conv_1"](x_peptide))
-        pep_out_1 = self.layers['peptide']["pool_1"](pep_out_1)
-        print(pep_out_1)
-        pep_out_3 = self.activation(self.layers['peptide']["conv_3"](x_peptide))
-        pep_out_3 = self.layers['peptide']["pool_3"](pep_out_3)
-        pep_out_5 = self.activation(self.layers['peptide']["conv_5"](x_peptide))
-        pep_out_5 = self.layers['peptide']["pool_5"](pep_out_5)
-        pep_out_7 = self.activation(self.layers['peptide']["conv_7"](x_peptide))
-        pep_out_7 = self.layers['peptide']["pool_7"](pep_out_7)
-        pep_out_9 = self.activation(self.layers['peptide']["conv_9"](x_peptide))
-        pep_out_9 = self.layers['peptide']["pool_9"](pep_out_9)
+        # cdrb_out_1 = self.activation(self.cdrb_conv[0](x_cdrb))
+        # cdrb_out_1 = self.pool(cdrb_out_1)
+        # cdrb_out_3 = self.activation(self.cdrb_conv[1](x_cdrb))
+        # cdrb_out_3 = self.pool(cdrb_out_3)
+        # cdrb_out_5 = self.activation(self.cdrb_conv[2](x_cdrb))
+        # cdrb_out_5 = self.pool(cdrb_out_5)
+        # cdrb_out_7 = self.activation(self.cdrb_conv[3](x_cdrb))
+        # cdrb_out_7 = self.pool(cdrb_out_7)
+        # cdrb_out_9 = self.activation(self.cdrb_conv[4](x_cdrb))
+        # cdrb_out_9 = self.pool(cdrb_out_9)
 
-        cdra_out_1 = self.activation(self.layers['cdra']["conv_1"](x_cdra))
-        cdra_out_1 = self.layers['cdra']["pool_1"](cdra_out_1)
-        cdra_out_3 = self.activation(self.layers['cdra']["conv_3"](x_cdra))
-        cdra_out_3 = self.layers['cdra']["pool_3"](cdra_out_3)
-        cdra_out_5 = self.activation(self.layers['cdra']["conv_5"](x_cdra))
-        cdra_out_5 = self.layers['cdra']["pool_5"](cdra_out_5)
-        cdra_out_7 = self.activation(self.layers['cdra']["conv_7"](x_cdra))
-        cdra_out_7 = self.layers['cdra']["pool_7"](cdra_out_7)
-        cdra_out_9 = self.activation(self.layers['cdra']["conv_9"](x_cdra))
-        cdra_out_9 = self.layers['cdra']["pool_9"](cdra_out_9)
-
-        cdrb_out_1 = self.activation(self.layers['cdrb']["conv_1"](x_cdrb))
-        cdrb_out_1 = self.layers['cdrb']["pool_1"](cdrb_out_1)
-        cdrb_out_3 = self.activation(self.layers['cdrb']["conv_3"](x_cdrb))
-        cdrb_out_3 = self.layers['cdrb']["pool_3"](cdrb_out_3)
-        cdrb_out_5 = self.activation(self.layers['cdrb']["conv_5"](x_cdrb))
-        cdrb_out_5 = self.layers['cdrb']["pool_5"](cdrb_out_5)
-        cdrb_out_7 = self.activation(self.layers['cdrb']["conv_7"](x_cdrb))
-        cdrb_out_7 = self.layers['cdrb']["pool_7"](cdrb_out_7)
-        cdrb_out_9 = self.activation(self.layers['cdrb']["conv_9"](x_cdrb))
-        cdrb_out_9 = self.layers['cdrb']["pool_9"](cdrb_out_9)
-
-        pep_out = torch.cat([pep_out_1, pep_out_3, pep_out_5, pep_out_7, pep_out_9], dim=1)
-        cdra_out = torch.cat([cdra_out_1, cdra_out_3, cdra_out_5, cdra_out_7, cdra_out_9], dim=1)
-        cdrb_out = torch.cat([cdrb_out_1, cdrb_out_3, cdrb_out_5, cdrb_out_7, cdrb_out_9], dim=1)
+        # pep_out = torch.cat([pep_out_1, pep_out_3, pep_out_5, pep_out_7, pep_out_9], dim=1)
+        # cdra_out = torch.cat([cdra_out_1, cdra_out_3, cdra_out_5, cdra_out_7, cdra_out_9], dim=1)
+        # cdrb_out = torch.cat([cdrb_out_1, cdrb_out_3, cdrb_out_5, cdrb_out_7, cdrb_out_9], dim=1)
 
         out = torch.cat([pep_out, cdra_out, cdrb_out], dim=1)
 
