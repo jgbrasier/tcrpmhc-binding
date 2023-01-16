b5b9fdaf8e1bfc7f1fb1a99759376ae97d583d92
JG Brasier <jbrasier@g.harvard.edu>
2023-01-16 19:59:56 +0100

    nettcr val metrics

--- scripts/nettcr.py
+++ scripts/nettcr.py
@@ -14,17 +14,16 @@ from src.utils import blosum50_full, blosum50_20aa
 
 
 tcrseq = TCRSeqDataModule(path_to_file='data/preprocessed/sample_train.csv',
-                        path_to_test_file='data/preprocessed/sample_test.csv',
                         peptide_len=9, cdra_len=30, cdrb_len=30, random_seed=12345)
 
-tcrseq.setup(sep=',', encoding=blosum50_20aa)
+tcrseq.setup(sep=',', encoding=blosum50_20aa, split='hard')
 train_loader = tcrseq.train_dataloader()
-test_loader = tcrseq.test_dataloader()
+val_loader = tcrseq.val_dataloader()
 
 nettcr = LightningNetTCR(peptide_len=9, cdra_len=30, cdrb_len=30, batch_size=tcrseq.hparams.batch_size)
 checkpoint_callback = ModelCheckpoint(dirpath=os.path.join('checkpoint','nettcr-data', 'nettcr'), save_top_k=1, monitor='val_loss')
 tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.path.join('logs','nettcr-data'), name='nettcr')
 trainer = pl.Trainer(max_epochs=20, logger=tb_logger, callbacks=[checkpoint_callback], log_every_n_steps=10)
 
-trainer.fit(nettcr, train_dataloaders=train_loader, val_dataloaders=test_loader)
+trainer.fit(nettcr, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
